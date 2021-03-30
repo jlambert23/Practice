879700b6477e914a1ad0fccf01bf4c2f6384a1dd
Jay <jb.lambert23@gmail.com>
2021-03-30 13:18:32 -0400

    kattis/orphanbackups

---
+++ Kattis/orphanbackups/index.js
@@ -0,0 +1,52 @@
+// node index.js < a-sample.in > a-sample.out
+// diff a-sample.out a-sample.ans
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+const images = new Map();
+const orphanFiles = [];
+let readingImages = true;
+
+rl.on('line', (line) => {
+  if (!line) {
+    readingImages = false;
+  } else if (readingImages) {
+    images.set(line, 0);
+  } else {
+    const splits = line.split('_');
+    const fileName = splits.slice(0, splits.length - 2).join('_');
+    const imageCount = images.get(fileName);
+    if (imageCount != null) {
+      images.set(fileName, imageCount + 1);
+    } else {
+      orphanFiles.push(line);
+    }
+  }
+});
+
+rl.on('close', () => {
+  const orphanImages = [];
+  for (let [image, count] of images) {
+    if (count === 0) {
+      orphanImages.push(image);
+    }
+  }
+
+  if (orphanFiles.length) {
+    orphanFiles.sort();
+    orphanFiles.forEach((file) => console.log(`F ${file}`));
+  }
+  if (orphanImages.length) {
+    orphanImages.sort();
+    orphanImages.forEach((image) => console.log(`I ${image}`));
+  }
+  if (!orphanFiles.length && !orphanImages.length) {
+    console.log('No mismatches.');
+  }
+});
