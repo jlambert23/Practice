d0ce245dd9e020b51ce2e32e12317fec875b126c
Jay Lambert <jb.lambert23@gmail.com>
2021-06-18 14:37:28 -0400

    Create mars-exploration.js

---
+++ HackerRank/mars-exploration.js
@@ -0,0 +1,48 @@
+// https://www.hackerrank.com/challenges/mars-exploration/problem
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'marsExploration' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+function marsExploration(s) {
+    const s2 = 'SOS'.repeat(s.length / 3);
+    return s.split('').filter((s, i) => s !== s2.charAt(i)).length;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = marsExploration(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
