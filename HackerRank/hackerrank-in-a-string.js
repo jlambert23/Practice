8a4cb634bdd4de505ba0c6f8201b740f24359c07
Jay Lambert <jb.lambert23@gmail.com>
2021-06-21 10:06:37 -0400

    Create hackerrank-in-a-string.js

---
+++ HackerRank/hackerrank-in-a-string.js
@@ -0,0 +1,57 @@
+// https://www.hackerrank.com/challenges/hackerrank-in-a-string
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'hackerrankInString' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function hackerrankInString(s) {
+    let h = 'hackerrank';
+    for (const c of s) {
+        if (c === h[0]) h = h.slice(1);
+        if (!h.length) break;
+    }
+    return h.length ? 'NO' : 'YES';
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        const result = hackerrankInString(s);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
