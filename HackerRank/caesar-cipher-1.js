be28df1d74ff9a852419d1ec171bd63f98d6327f
Jay Lambert <jb.lambert23@gmail.com>
2021-09-29 17:01:35 -0400

    Create caesar-cipher-1.js

---
+++ HackerRank/caesar-cipher-1.js
@@ -0,0 +1,59 @@
+// https://www.hackerrank.com/challenges/caesar-cipher-1
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'caesarCipher' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. INTEGER k
+ */
+
+function caesarCipher(s, k) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    return [...s].map((c) => {
+        const index = alphabet.indexOf(c.toLowerCase());
+        if (index < 0) { return c; }
+        const newChar = alphabet[(index + k) % alphabet.length];
+        return c === c.toUpperCase() ? newChar.toUpperCase() : newChar;
+    }).join('');
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const s = readLine();
+
+    const k = parseInt(readLine().trim(), 10);
+
+    const result = caesarCipher(s, k);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
