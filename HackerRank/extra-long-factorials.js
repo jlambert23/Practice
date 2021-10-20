dd626c5cffa4dc61e636bd685659a8da751656d4
Jay Lambert <jb.lambert23@gmail.com>
2021-10-20 10:37:42 -0400

    Create extra-long-factorials.js

---
+++ HackerRank/extra-long-factorials.js
@@ -0,0 +1,42 @@
+// https://www.hackerrank.com/challenges/extra-long-factorials/problem
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'extraLongFactorials' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function extraLongFactorials(n) {
+    let res = 1n;
+    for (let i = n; i > 1; i--) {
+       res *= BigInt(i);
+    }
+    console.log(res.toString());
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    extraLongFactorials(n);
+}
